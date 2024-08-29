c73c0a8c197d0c6d006f2805b690c8416b974ce7
Vasco Ceti <vasco.ceti@gmail.com>
2024-08-29 10:11:36 +0200

    food cost init

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [total, setTotal] = useState<number>(0);
+  const [consumed, setConsumed] = useState<number>(0);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const quantity = (document.getElementById("quantity") as HTMLInputElement)
+      .value;
+    const pricekg = (document.getElementById("pricekg") as HTMLInputElement)
+      .value;
+    const stock = (document.getElementById("stock") as HTMLInputElement).value;
+
+    const total = (+quantity * +pricekg) / 1000;
+    const consumed = (+stock * +pricekg) / 1000;
+
+    setTotal(total);
+    setConsumed(consumed);
+    // const costFinal = cost - costStock;
+    // dopo total vai a capo
+    alert(`Il costo totale è 
+      ${total}€ ,
+
+  Il costo della quantità consumata è 
+  ${consumed}€`);
+  };
+
+  const prevResultConditopn = !!total || !!consumed;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+          gap: "1rem",
+        }}
+      >
+        <h1>Food cost</h1>
+        {prevResultConditopn && (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              backgroundColor: "lightgray",
+              padding: "1rem",
+              borderRadius: "0.5rem",
+            }}
+          >
+            <h3>Ultimo calcolo effettuato</h3>
+            <p>Il costo totale è {total.toFixed(2)}€</p>
+            <p>il costo consumato è {consumed.toFixed(2)}€</p>
+          </div>
+        )}
+        <label htmlFor="quantity">Inserisci la quantità totale in grammi</label>
+        <input id="quantity" type="number" />
+        <label htmlFor="pricekg">Inserisci il prezzo al chilo</label>
+        <input id="pricekg" type="number" step={0.01} required />
+        <label htmlFor="stock">Inserisci la quantità consumata in grammi</label>
+        <input id="stock" type="number" />
+        <div
+          style={{
+            display: "flex",
+            gap: "1rem",
+          }}
+        >
+          <button
+            style={{
+              padding: "0.5rem 1rem",
+              backgroundColor: "red",
+              color: "white",
+              border: "none",
+              borderRadius: "0.5rem",
+            }}
+            onClick={() => {
+              setTotal(0);
+              setConsumed(0);
+            }}
+            type="reset"
+          >
+            reset
+          </button>
+          <button
+            type="submit"
+            style={{
+              padding: "0.5rem 1rem",
+              backgroundColor: "blue",
+              color: "white",
+              border: "none",
+              borderRadius: "0.5rem",
+            }}
+          >
+            calcola
+          </button>
+        </div>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
